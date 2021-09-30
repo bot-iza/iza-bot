d7a6ffc467895958ed697d368129cb8f3e5765e8
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-30 01:46:19 -0300

    Add files via upload

--- src/help.js
+++ src/help.js
@@ -1,7 +1,41 @@
-const help = (prefix) => {
-	return ` 
-	menu estão ainda desenvolvendo 
-`
-}
+const help = (prefix, sender, pushname, time, frasealeatoria) => {
 
+	return ` 
+┏━━━━━━━━━━━━━━━━━━━━
+┃─────〘 LENDARIO 〙────
+┃━━━━━━━━━━━━━━━━━━━━
+┠⊷ nome: ${pushname}
+┠⊷ numéro wa.me/${sender.split("@")[0]}   
+┠⊷ tempo ativo: ${kyun(uptime)}
+┠⊷ Velocidade: ${process.uptime()}  
+┠⊷ Data e hora: ${time}   
+┠⊷ frase: ${frasealeatoria}
+┗━━━━━━━━━━━━━━━━━━━━  
+ ┏━━━━━━━━━━━━━━━━━━━━
+ ┃─────〘 MENU 〙─────
+ ┗━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━          
+┠⊷ ${prefix}admins                    
+┠⊷ ${prefix}usuario        
+┠⊷ ${prefix}dono                           
+┠⊷ ${prefix}novidade
+┠⊷ ${prefix}diversao
+┗━━━━━━━━━━━━━━━━━━━━
+ ┏━━━━━━━━━━━━━━━━━━━━
+ ┃─────〘 LєиdαяiσBσт 〙─────
+ ┗━━━━━━━━━━━━━━━━━━━━ 
+
+      `
+function kyun(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var Horas = Math.floor(seconds / (60*60));
+  var Minutos = Math.floor(seconds % (60*60) / 60);
+  var Segundos = Math.floor(seconds % 60);
+
+  //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
+  return `${pad(Horas)} Horas ${pad(Minutos)} Minutos ${pad(Segundos)} Segundos`
+}
+}
 exports.help = help
