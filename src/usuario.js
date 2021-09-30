1e7c291daeb87b66c1f788670dad1c9124c94781
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-30 02:21:52 -0300

    Update usuario.js

--- src/usuario.js
+++ src/usuario.js
@@ -3,14 +3,10 @@ const usuario = (prefix, sender) => {
 
 	return ` 
  ┏━━━━━━━━━━━━━━━━━━━━
- ┃─────〘 LєиdαяiσBσт 〙─────
+ ┃─────〘 Bσт-Riquɛ 〙─────
  ┗━━━━━━━━━━━━━━━━━━━━
 ┏━━━━━━━━━━━━━━━━━━━━          
 ┠⊷ ${prefix}marcar                    
-┠⊷ ${prefix}rick (dono do bot)	          
-┠⊷ ${prefix}meunumero                           
-┠⊷ ${prefix}voz pt
-┠⊷ ${prefix}ler
 ┠⊷ ${prefix}prefixo
 ┠⊷ ${prefix}listonline
 ┠⊷ ${prefix}velocidade
@@ -20,44 +16,23 @@ const usuario = (prefix, sender) => {
 ┗━━━━━━━━━━━━━━━━━━━━
 ┏━━━━━━━━━━━━━━━━━━━━  
 ┠⊷ ${prefix}musica
-┠⊷ ${prefix}youtube
-┠⊷ ${prefix}ytmp3
 ┗━━━━━━━━━━━━━━━━━━━━━    
-┏━━━━━━━━━━━━━━━━━━━━
-┃─────〘 IMAGEM 〙────
-┗━━━━━━━━━━━━━━━━━━━━     
-┏━━━━━━━━━━━━━━━━━━━━ 
-┠⊷ ${prefix}imagem  
-┠⊷ ${prefix}frases         
-┠⊷ ${prefix}boanoite      
-┠⊷ ${prefix}bomdia        
-┠⊷ ${prefix}boatarde              
-┠⊷ ${prefix}meme                  
-┠⊷ ${prefix}dbz
-┗━━━━━━━━━━━━━━━━━━━━    
+
 ┏━━━━━━━━━━━━━━━━━━━━
 ┃────〘 STICKER 〙───
 ┗━━━━━━━━━━━━━━━━━━━━ 
 ┏━━━━━━━━━━━━━━━━━━━━   
-┠⊷ ${prefix}converte
 ┠⊷ ${prefix}figurinha
 ┠⊷ ${prefix}fig
 ┠⊷ ${prefix}s
 ┗━━━━━━━━━━━━━━━━━━━━  
-┏━━━━━━━━━━━━━━━━━━━━
-┃─〘 INTELIGÊNCIA 〙─
-┗━━━━━━━━━━━━━━━━━━━━
-┏━━━━━━━━━━━━━━━━━━━━    
-┠⊷ ${prefix}conversa 1            
-┠⊷ ${prefix}conversa 0            
-┠⊷ ${prefix}simi     
-┗━━━━━━━━━━━━━━━━━━━━  
+ 
 ┏━━━━━━━━━━━━━━━━━━━━
 ┠⊷ Mᴇᴜ ᴄʀɪᴀᴅᴏʀ:
 ┠⊷ wa.me/554891463194
 ┠⊷║▌│█║▌│ █║▌│█│║▌║
 ┠⊷║▌│█║▌│ █║▌│█│║▌║
-┠⊷ Copyright ® Bσт σ Lєиdαяiσ  
+┠⊷ Copyright ® Bσт-Riquɛ 
 ┗━━━━━━━━━━━━━━━━━━━━
  `
 }
