78c00249f124522e4ef3dbaa2aa95dcff465fadb
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-30 22:22:19 -0300

    Update diversao.js

--- src/diversao.js
+++ src/diversao.js
@@ -33,7 +33,45 @@ const diversao = (prefix, sender, pushname, time) => {
 ┠⊷ wa.me/554891463194
 ┠⊷║▌│█║▌│ █║▌│█│║▌║
 ┠⊷║▌│█║▌│ █║▌│█│║▌║
-┠⊷ Copyright ® Bσт σ Lєиdαяiσ 2021 
+┠⊷ Copyright ® const diversao = (prefix, sender, pushname, time) => {
+
+	return`
+┏━━━━━━━━━━━━━━━━━━━━
+┃─────〘 DIVERSÃO 〙────
+┃━━━━━━━━━━━━━━━━━━━━
+┠⊷ nome: ${pushname}   
+┠⊷ Velocidade: ${process.uptime()}  
+┠⊷━━━━━━━━━━━━━━━━━━━━                              
+┠⊷ ${prefix}bass 
+┠⊷ ${prefix}sn
+┠⊷ ${prefix}feios
+┠⊷ ${prefix}golpe
+┠⊷ ${prefix}gadometro
+┠⊷ ${prefix}retardado
+┠⊷ ${prefix}casal
+┠⊷ ${prefix}gays
+┠⊷ ${prefix}cornos
+┠⊷ ${prefix}gostosas
+┠⊷ ${prefix}poesia
+┠⊷ ${prefix}chance
+┠⊷ ${prefix}nicks
+┠⊷ ${prefix}gay1
+┠⊷ ${prefix}wiki 
+┠⊷ ${prefix}punheteiros
+┠⊷ ${prefix}humor
+┠⊷ ${prefix}bichas
+┠⊷ ${prefix}cassino
+┗━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━
+┠⊷ Mᴇᴜ ᴄʀɪᴀᴅᴏʀ:
+┠⊷ wa.me/554891463194
+┠⊷║▌│█║▌│ █║▌│█│║▌║
+┠⊷║▌│█║▌│ █║▌│█│║▌║
+┠⊷ Copyright ® Bσт-ʀiquɛ 
+┗━━━━━━━━━━━━━━━━━━━━
+`
+}
+exports.diversao = diversao
 ┗━━━━━━━━━━━━━━━━━━━━
 `
 }
