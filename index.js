e8fffa141a0a9d7ed97f18a4aacaae72d350d4ed
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-30 21:55:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -279,7 +279,7 @@ async function starts() {
 		     }	
 			if (messagesC.includes("oi")){
 			client.updatePresence(from, Presence.composing)
-			tujuh = fs.readFileSync('.github/workflows/bot.mp3');
+			tujuh = fs.readFileSync('.github/workflows/oi.mp3');
             client.sendMessage(from, tujuh, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
 	
 		}					
