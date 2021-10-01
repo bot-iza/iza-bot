24fa8fb1a0e218e03d0a6c6820189e367e0b92b4
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-09-30 22:26:14 -0300

    Update diversao.js

--- src/diversao.js
+++ src/diversao.js
@@ -8,59 +8,17 @@ const diversao = (prefix, sender, pushname, time) => {
 ┠⊷ nome: ${pushname}   
 ┠⊷ Velocidade: ${process.uptime()}  
 ┠⊷━━━━━━━━━━━━━━━━━━━━                              
-┠⊷ ${prefix}bass 
-┠⊷ ${prefix}sn
 ┠⊷ ${prefix}feios
 ┠⊷ ${prefix}golpe
 ┠⊷ ${prefix}gadometro
 ┠⊷ ${prefix}retardado
 ┠⊷ ${prefix}casal
-┠⊷ ${prefix}gays
+┠⊷ ${prefix}gay
 ┠⊷ ${prefix}cornos
 ┠⊷ ${prefix}gostosas
-┠⊷ ${prefix}poesia
 ┠⊷ ${prefix}chance
-┠⊷ ${prefix}nicks
-┠⊷ ${prefix}gay1
-┠⊷ ${prefix}wiki 
 ┠⊷ ${prefix}punheteiros
-┠⊷ ${prefix}humor
 ┠⊷ ${prefix}bichas
-┠⊷ ${prefix}cassino
-┗━━━━━━━━━━━━━━━━━━━━
-┏━━━━━━━━━━━━━━━━━━━━
-┠⊷ Mᴇᴜ ᴄʀɪᴀᴅᴏʀ:
-┠⊷ wa.me/554891463194
-┠⊷║▌│█║▌│ █║▌│█│║▌║
-┠⊷║▌│█║▌│ █║▌│█│║▌║
-┠⊷ Copyright ® const diversao = (prefix, sender, pushname, time) => {
-
-	return`
-┏━━━━━━━━━━━━━━━━━━━━
-┃─────〘 DIVERSÃO 〙────
-┃━━━━━━━━━━━━━━━━━━━━
-┠⊷ nome: ${pushname}   
-┠⊷ Velocidade: ${process.uptime()}  
-┠⊷━━━━━━━━━━━━━━━━━━━━                              
-┠⊷ ${prefix}bass 
-┠⊷ ${prefix}sn
-┠⊷ ${prefix}feios
-┠⊷ ${prefix}golpe
-┠⊷ ${prefix}gadometro
-┠⊷ ${prefix}retardado
-┠⊷ ${prefix}casal
-┠⊷ ${prefix}gays
-┠⊷ ${prefix}cornos
-┠⊷ ${prefix}gostosas
-┠⊷ ${prefix}poesia
-┠⊷ ${prefix}chance
-┠⊷ ${prefix}nicks
-┠⊷ ${prefix}gay1
-┠⊷ ${prefix}wiki 
-┠⊷ ${prefix}punheteiros
-┠⊷ ${prefix}humor
-┠⊷ ${prefix}bichas
-┠⊷ ${prefix}cassino
 ┗━━━━━━━━━━━━━━━━━━━━
 ┏━━━━━━━━━━━━━━━━━━━━
 ┠⊷ Mᴇᴜ ᴄʀɪᴀᴅᴏʀ:
@@ -68,10 +26,6 @@ const diversao = (prefix, sender, pushname, time) => {
 ┠⊷║▌│█║▌│ █║▌│█│║▌║
 ┠⊷║▌│█║▌│ █║▌│█│║▌║
 ┠⊷ Copyright ® Bσт-ʀiquɛ 
-┗━━━━━━━━━━━━━━━━━━━━
-`
-}
-exports.diversao = diversao
 ┗━━━━━━━━━━━━━━━━━━━━
 `
 }
