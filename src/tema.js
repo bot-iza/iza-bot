86de253180655a88a2c581275fc10ed554513baf
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-10-07 22:34:31 -0300

    Create tema.js

---
+++ src/tema.js
@@ -0,0 +1,27 @@
+const tema = (prefix, sender, pushname, time) => {
+
+	return`
+┏━━━━━━━━━━━━━━━━━━━━
+┃─────〘 TEMA 〙────
+┃━━━━━━━━━━━━━━━━━━━━
+┠⊷ nome: ${pushname}
+┠⊷ Velocidade: ${process.uptime()}  
+┠⊷━━━━━━━━━━━━━━━━━━━━                              
+┠⊷ ${prefix}imagem (txt)
+┠⊷ ${prefix}minion (txt)
+┠⊷ ${prefix}bear (txt)
+┠⊷ ${prefix}joker(txt)
+┠⊷ ${prefix}cs-go (txt)
+┠⊷ ${prefix}logotop (txt)
+┠⊷ ${prefix}matrix (txt)
+┗━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━
+┠⊷ Mᴇᴜ ᴄʀɪᴀᴅᴏʀ:
+┠⊷ wa.me/554891463194
+┠⊷║▌│█║▌│ █║▌│█│║▌║
+┠⊷║▌│█║▌│ █║▌│█│║▌║
+┠⊷ Copyright ® Bσт   
+┗━━━━━━━━━━━━━━━━━━━━
+`
+}
+exports.tema = tema
