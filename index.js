88062f9c102b28ddadb80865207766b17a99508d
bot-rique21 <90358090+bot-rique21@users.noreply.github.com>
2021-10-07 23:35:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -377,12 +377,13 @@ async function starts() {
  }
 break
 					
-					case 'saycat': 
+					case 'saycat':
+					 reply('⌛ aguarde Enviado video ...⌛')
 data = await fetchJson(`https://pastebin.com/raw/cVDj0qz6`)
 n = JSON.parse(JSON.stringify(data));
 nimek = n[Math.floor(Math.random() * n.length)];
 buffer = await getBuffer(nimek.result)
-client.sendMessage(from, buffer, video, {mimetype: 'video/mp4',quoted: mek, caption: `Amo saycat❤️`})
+client.sendMessage(from, buffer, video, {mimetype: 'video/mp4',quoted: mek, caption: `pra quem é fã saycat❤️`})
  break
 					
 					case 'ler':
