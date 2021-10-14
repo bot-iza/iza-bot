610d3f212b3b7a13df615d0d3f2ff1a8ab1fb23b
bot-rique-ofc <90358090+bot-rique21@users.noreply.github.com>
2021-10-13 22:43:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1229,6 +1229,24 @@ case 'gays':
 					membr.push(gays5.jid)
 					mentions(teks, membr, true)
 					break
+					
+					case 'amor':
+                if (mek.message.extendedTextMessage === undefined || mek.message.extendedTextMessage === null) return reply('Marque a pessoa')
+				mentidn = mek.message.extendedTextMessage.contextInfo.mentionedJid[0]
+                ghost = mek.participant
+                const mor =['05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58 ','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','92','93','94','95','96','97','98','99','100']
+				const am = mor[Math.floor(Math.random() * mor.length)]
+				rate = body.slice(1)		
+				var kic = `${sender.split("@")[0]}@s.whatsapp.net`		
+		     	 reply(`Fazendo probabilidades`)
+					data = fs.readFileSync('./integraçao/shit.js');
+                 jsonData = JSON.parse(data);
+                 randIndex = Math.floor(Math.random() * jsonData.length);
+                 randKey = jsonData[randIndex];
+                buffer = await getBuffer(randKey.result)               
+                amor = `${pushname} suas chances de ficar com @${mentidn.split('@')[0]} são de: ${am}%`
+                client.sendMessage(from, buffer, video, {mimetype: 'video/mp4', quoted: mek, caption: amor, contextInfo: {mentionedJid: [mentidn]}})
+				break 
 
 //ACABOU A DIVERSÃO
 				
