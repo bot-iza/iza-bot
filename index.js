0eabfaf5fe3b312c20ee601c993f25e4dca2864d
bot-rique-ofc <90358090+bot-rique21@users.noreply.github.com>
2021-10-15 18:26:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ async function starts() {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `Menos 1 😔... @${num.split('@')[0]}`
+				teks = `QUE PENA SAIU MAIS UM FAMILIA 😔... @${num.split('@')[0]}`
 				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			}
