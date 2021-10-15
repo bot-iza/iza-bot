b124885de4e6fe4ebac353524be36cc9c0c84b5a
bot-rique-ofc <90358090+bot-rique21@users.noreply.github.com>
2021-10-15 18:00:09 -0300

    Create apks.js

---
+++ src/apks.js
@@ -0,0 +1,13 @@
+const apks = (prefix, send) => {
+       return ` LISTA DE APK MODIFICADO 
+       
+  picsart
+ https://apkadmin.com/mmfn0qv0726p/PicsArt_Gold_Full_v17.1.4.apk.html
+       
+       
+       IZA-BOT DOMINA 
+       
+`
+}
+
+exports.imunes = imunes
