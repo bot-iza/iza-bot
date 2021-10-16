fddd09e2c7bbacc8cdf3c9313ec13edcb6181e95
bot-rique-ofc <90358090+bot-rique21@users.noreply.github.com>
2021-10-15 21:42:25 -0300

    Create trap.js

---
+++ src/trap.js
@@ -0,0 +1,12 @@
+[
+{"result": "https://www.mboxdrive.com/y2meta.com%20-%20Menor%20MC%20-%20Tipo%20Gringa%20feat.%20DJ%20Matt-D%20(128%20kbps).mp3"},
+{"result": "https://www.mboxdrive.com/y2meta.com%20-%20Orochi%20%E2%80%9CAt%C3%A9%20de%20Manh%C3%A3%E2%80%9C%20(prod.%20jess,%20Ajaxx,%20Kizzy)%20(128%20kbps).mp3"},
+{"result": "https://www.mboxdrive.com/y2meta.com%20-%20MATU%C3%8A%20-%20QUER%20VOAR%20%F0%9F%A9%B8%20(128%20kbps).mp3"},
+{"result": "https://www.mboxdrive.com/y2meta.com%20-%20MC%20Poze%20do%20Rodo%20-%20Vida%20Louca%20(prod.%20Neobeats)%20(128%20kbps).mp3"},
+{"result": "https://www.mboxdrive.com/y2meta.com%20-%20Filipe%20Ret%20-%20Corte%20Americano%20part.%20L7NNON%20(128%20kbps).mp3"},
+{"result": "https://www.mboxdrive.com/y2meta.com%20-%20TZ%20DA%20CORONEL%20-%20ANOTA%20PLACA%20(%C3%81UDIO%20OFICIAL)%20(128%20kbps).mp3"},
+{"result": "https://www.mboxdrive.com/y2meta.com%20-%20L7NNON%20-%20Freio%20da%20Blazer%20%F0%9F%9A%A8%20(prod.%20Ajaxx%20&%20Galdino)%20(128%20kbps).mp3"},
+{"result": "https://www.mboxdrive.com/y2meta.com%20-%20Orochi%20%E2%80%9CCELEBRIDADE%E2%80%9C%20%F0%9F%8C%8E%20(Prod.%20Kizzy)%20(128%20kbps).mp3"},
+{"result": "https://www.mboxdrive.com/y2meta.com%20-%20Orochi%20BAL%C3%83O%20%F0%9F%8E%88%20(Prod.%20Dallass)%20(128%20kbps).mp3"},
+{"result": "https://www.mboxdrive.com/y2meta.com%20-%20Teto%20-%20M4%20(Letra)%20ft.%20Matu%C3%AA%20(m4%20gritando%20o%20meu%20nome)%20(128%20kbps).mp3"}
+]
