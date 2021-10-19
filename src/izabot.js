e7e5b4f69bba461408d3e54216fbe52f278a4afa
iza-bot-ofc <92641822+bot-iza@users.noreply.github.com>
2021-10-19 14:56:28 -0300

    Update izabot.js

--- src/izabot.js
+++ src/izabot.js
@@ -1,1 +1,24 @@
 
+const izabot = (prefix, sender, pushname, time) => {
+
+	return`
+┏━━━━━━━━━━━━━━━━━━━━
+┃──〘 BOT FALAR 〙───
+┗━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━      
+┠⊷ bot
+┠⊷ ${prefix}tchau
+┠⊷ ${prefix}bomdia
+┠⊷ ${prefix}boatarde
+┠⊷ ${prefix}boanoite
+┗━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━
+┠⊷ Mᴇᴜ ᴄʀɪᴀᴅᴏʀ:
+┠⊷ wa.me/554891463194
+┠⊷║▌│█║▌│ █║▌│█│║▌║
+┠⊷║▌│█║▌│ █║▌│█│║▌║
+┠⊷ Copyright ® IZA-BOT
+┗━━━━━━━━━━━━━━━━━━━━
+`
+}
+exports.izabot =izabot
