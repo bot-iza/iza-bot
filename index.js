c5d8897d65bceacd8999c82068d005d7e1ccd5b8
iza-bot-ofc <92641822+bot-iza@users.noreply.github.com>
2021-11-03 14:52:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -68,40 +68,6 @@ async function starts() {
 	})
 	await client.connect({timeoutMs: 30*1000})
         fs.writeFileSync('./BarBar.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
-	//ANTI-PALAVRAO
-			
-			if (budy.includes("filho da puta")){
-		if (!isGroup) return
-		if (!isAntiPalavra) return
-		if (isGroupAdmins) return reply('porque você é um administrador do grupo, os bot não grupo ')
-		client.updatePresence(from, Presence.composing)
-		if (messagesC.includes("#harusizintod")) return reply("izin diterima")
-		var kic = `${sender.split("@")[0]}@s.whatsapp.net`
-		reply(`「 PALAVRÃO DETECTADO 」\n${sender.split("@")[0]} vou remover por falar palavrão no grupo *`)
-		setTimeout( () => {
-			client.groupRemove(from, [kic]).catch((e)=>{reply(`ERR: ${e}`)})
-		}, 1000)
-		setTimeout( () => {
-			client.updatePresence(from, Presence.composing)
-		
-		}, 0)
-	}
-			if (budy.includes("foda-se")){
-		if (!isGroup) return
-		if (!isAntiPalavra) return
-		if (isGroupAdmins) return reply('porque você é um administrador do grupo, e o bot  não vai te remover')
-		client.updatePresence(from, Presence.composing)
-		if (messagesC.includes("#harusizintod")) return reply("izin diterima")
-		var kic = `${sender.split("@")[0]}@s.whatsapp.net`
-		setTimeout( () => {
-			client.groupRemove(from, [kic]).catch((e)=>{reply(`ERR: ${e}`)})
-		}, 1000)
-		setTimeout( () => {
-			client.updatePresence(from, Presence.composing)
-		
-		}, 0)
-	
-	
 	
 	
 	//**** ANTI-FAKE *****
@@ -624,7 +590,7 @@ client.sendMessage(from, buffer, video, {mimetype: 'video/mp4',quoted: mek, capt
   client.sendMessage(from, florr, image, {quoted: mek, caption: claa})
   break
 					
-					case 'iza':
+					case 'isa':
 					if (args.length < 1) return reply('Onde está o texto, hum?')
 					teks = body.slice(5)
 					anu = await simih(teks) //fetchJson(`https://mhankbarbars.herokuapp.com/api/samisami?text=${teks}`, {method: 'get'})
